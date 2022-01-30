cbba9521e1b0aa31feca0995d7f7801b89abc4b0
Alice <alice.dvache17@gmail.com>
2022-01-30 15:08:12 +0500

    Movement, rogue-like

--- text_game.py
+++ text_game.py
@@ -1,24 +1,29 @@
 import inspect
 import json
 import logging
+import attr
+import math
 import random
+from abc import ABC, abstractmethod
 from typing import Any, Callable
 
 
+PI_HALF = 1.5707963267948966
+
+
 class Game:
+    @attr.define
     class Position:
-        def __init__(self):
-            self.x = 0
-            self.y = 0
+        x: int = 0
+        y: int = 0
 
-    class Location:
-        class LocationType:
-            def __init__(self, name: str):
-                self.name = name
+    @attr.define
+    class Location(ABC):
+        name: str
 
-        def __init__(self, name: str, type_: LocationType):
-            self.name = name
-            self.type = type_
+    @attr.define
+    class LocationCity(Location):
+        pass
 
     ### General methods
 
@@ -38,32 +43,58 @@ class Game:
             else:
                 print(self.text_pool["yes_no_invalid_input"])
 
+    def movement(self, x: int = 0, y: int = 0):
+        if self.rogue_like or f"{self.position.x + x}; {self.position.y + y}" in self.map:
+            self.position.x += x
+            self.position.y += y
+            self.update_map()
+
+    def rotation_movement(self, distance: float, angle: float, angle_in_degrees: bool = True):
+        if angle_in_degrees:
+            angle = math.radians(angle)
+        self.movement(round(math.cos(angle - PI_HALF)*distance),
+                      round(math.sin(angle + PI_HALF)*distance))
+
     @property
     def map_coords(self):
         return f"{self.position.x}; {self.position.y}"
 
     def update_map(self):
-        if self.map_coords not in self.map:
+        if self.rogue_like and self.map_coords not in self.map:
             self.map[self.map_coords] = random.choice(self.location_pool)
 
     ### Commands
 
     ## Movement
+    # 90 degrees
     def north(self):
-        self.position.x += 1
-        self.update_map()
+        self.rotation_movement(1, 0)
+        #self.movement(0, 1)
 
     def south(self):
-        self.position.x -= 1
-        self.update_map()
+        self.rotation_movement(1, 180)
+        #self.movement(0, -1)
 
     def east(self):
-        self.position.y += 1
-        self.update_map()
+        self.rotation_movement(1, 90)
+        #self.movement(1, 0)
 
     def west(self):
-        self.position.y -= 1
-        self.update_map()
+        self.rotation_movement(1, 270)
+        #self.movement(-1, 0)
+
+    # 45 degrees
+    def north_east(self):
+        self.rotation_movement(2, 45)
+
+    def south_east(self):
+        self.rotation_movement(2, 135)
+
+    def south_west(self):
+        self.rotation_movement(2, 225)
+
+    def north_west(self):
+        self.rotation_movement(2, 315)
 
     ## Info
     def get_position(self):
@@ -92,40 +123,71 @@ class Game:
     def set_logging_level(self, logging_level: int):
         logging.basicConfig(level=logging_level, force=True)
 
+    def antigravity(self):
+        import antigravity
+
     ### Game methods
 
     def __init__(self):
-        self.command_pool = {
-                             # System
-                             "exit": self.exit,
-                             "gll": self.get_logging_level,
-                             "sll": self.set_logging_level,
-                             # Movement
-                             "n": self.north,
-                             "north": self.north,
-                             "s": self.south,
-                             "south": self.south,
-                             "w": self.west,
-                             "west": self.west,
-                             "e": self.east,
-                             "east": self.east,
-                             # Info
-                             "gp": self.get_position,
-                             "getpos": self.get_position,
-                             "get_position": self.get_position,
-                             "gl": self.get_location,
-                             "getloc": self.get_location,
-                             "get_location": self.get_location,
-                             "sm": self.show_map,
-                             "show_map": self.show_map
-                            }
+        system_commands = {
+            "exit": self.exit,
+            "gll": self.get_logging_level,
+            "sll": self.set_logging_level,
+            "ag": self.antigravity,
+            "antigrav": self.antigravity,
+            "antigravity": self.antigravity,
+        }
+        debug_commands = {
+            "mv": self.movement,
+            "movement": self.movement,
+            "rmv": self.rotation_movement,
+            "rotation_movement": self.rotation_movement,
+        }
+        movement_commands = {
+            # 90 degrees
+            "n": self.north,
+            "north": self.north,
+            "s": self.south,
+            "south": self.south,
+            "w": self.west,
+            "west": self.west,
+            "e": self.east,
+            "east": self.east,
+            # 45 degrees
+            "ne": self.north_east,
+            "north_east": self.north_east,
+            "se": self.south_east,
+            "south_east": self.south_east,
+            "sw": self.south_west,
+            "south_west": self.south_west,
+            "nw": self.north_west,
+            "north_west": self.north_west,
+        }
+        info_commands = {
+            "gp": self.get_position,
+            "getpos": self.get_position,
+            "get_position": self.get_position,
+            "gl": self.get_location,
+            "getloc": self.get_location,
+            "get_location": self.get_location,
+            "sm": self.show_map,
+            "show_map": self.show_map
+        }
+        self.command_pool = dict(
+            **system_commands,
+            **debug_commands,
+            **movement_commands,
+            **info_commands
+        )
 
-        with open("./translations/ru_RU.json", encoding="utf-8") as f:
-            self.text_pool = json.load(f)
+        with open("./translations/ru_RU.json", encoding="utf-8") as json_file:
+            self.text_pool = json.load(json_file)
+
+        self.rogue_like = True
 
         self.location_pool = (
-            self.Location("City", self.Location.LocationType("city")),
-            self.Location("Forest", self.Location.LocationType("forest"))
+            self.Location("City"),
+            self.Location("Forest")
         )
 
         self.game_active = True
@@ -161,7 +223,8 @@ class Game:
                         break
                 else:
                     self.command_pool[command](*args)
-                    logging.debug("Executed %s with arguments: %s", self.command_pool[command], *args)
+                    logging.debug("Executed %s with arguments: %s",
+                                  self.command_pool[command], *args)
             else:
                 print(self.text_pool["wrong_arguments_amount"], len(expected_args))
         else:
